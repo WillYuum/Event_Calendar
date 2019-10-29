457ea2a08221809fec2e05ff62ae31929a88de08
William <liams_1998@hotmail.com>
2019-10-29 19:56:35 +0200

    fetched events from backend

--- front/App.js
+++ front/App.js
@@ -1,19 +1,63 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>DIE PERLA</Text>
-    </View>
-  );
-}
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#000000",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
+
+const text = StyleSheet.create({
+  txt: {
+    color: "#fff"
+  }
+});
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Events: [],
+      fakeData: [
+        { Name: "WIlly", FamilyName: "Abou" },
+        { Name: "DADDY", FamilyName: "faller" },
+        { Name: "cheese", FamilyName: "mac" }
+      ]
+    };
+  }
+  componentDidMount() {
+    this.getEvents();
+  }
+
+  getEvents = async () => {
+    try {
+      const req = await fetch("http://192.168.49.113:3000/events", {
+        method: "GET"
+      });
+      const res = await req.json();
+      console.log(res);
+      this.setState({ Events: res.rows });
+    } catch (err) {
+      throw new Error(`failed fetching events with = ${err}`);
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.state.Events.map((event, index) => {
+          return (
+            <Text key={index} style={text.txt}>
+              {event.doc.Eventname}
+            </Text>
+          );
+        })}
+      </View>
+    );
+  }
+}
+
+export default App;
